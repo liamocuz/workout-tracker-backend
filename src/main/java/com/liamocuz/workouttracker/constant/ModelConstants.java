edd79c1dce3020df45d213108d2b57c5b3ef1d7a
liamocuz <liamo@me.com>
2024-06-06 20:43:03 -0400

    Placeholder constants

---
+++ src/main/java/com/liamocuz/workouttracker/constant/ModelConstants.java
@@ -0,0 +1,8 @@
+package com.liamocuz.workouttracker.constant;
+
+public class ModelConstants {
+    public final static int DEFAULT_STRING_LENGTH = 255;
+    public final static int DEFAULT_DESCRIPTION_LENGTH = 1000;
+    public final static int MAX_WORKOUT_TEMPLATES = 10;
+    public final static int MAX_EXERCISE_TEMPLATES = 20;
+}
