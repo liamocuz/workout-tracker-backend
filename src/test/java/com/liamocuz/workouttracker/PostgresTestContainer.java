7c42f2e046a19dc8948dfacab95e7ccc579ebdf6
liamocuz <liamo@me.com>
2024-06-09 22:15:56 -0400

    Using an interface to import with test containers

---
+++ src/test/java/com/liamocuz/workouttracker/PostgresTestContainer.java
@@ -0,0 +1,21 @@
+package com.liamocuz.workouttracker;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public interface PostgresTestContainer {
+
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:alpine");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
