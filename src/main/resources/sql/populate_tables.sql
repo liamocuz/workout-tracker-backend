ba2096952d1deb0fb38c7437c009abecd2a0c430
liamocuz <liamo@me.com>
2024-06-11 21:25:51 -0400

    Deleted

--- src/main/resources/sql/populate_tables.sql
+++
@@ -1,6 +0,0 @@
--- Populate the Strength Exercise Type table
-INSERT INTO strength_exercise_type (name, muscle_group)
-VALUES ('Bench Press', 'CHEST');
-
-INSERT INTO strength_exercise_type (name, muscle_group)
-VALUES ('Lat Pulldown', 'BACK');
